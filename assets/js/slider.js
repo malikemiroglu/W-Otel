ed6650bcd5e8daaab18b302a5e9578e9f767ccf4
Malik <malikemiroglu92@gmail.com>
2024-09-03 23:00:23 +0300

    Rooms completed.

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -68,3 +68,32 @@ dots.forEach((dot, idx) => {
 
 showSlide(0);
 // startAutoSlide();
+
+// ROOMS SCROLL
+const slider = document.getElementById("rooms-container");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2;
+  slider.scrollLeft = scrollLeft - walk;
+});
